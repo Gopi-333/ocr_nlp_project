b3d2c139c829f420fc592a8ea0ad7eac52dec787
Gopi-333 <gopiramana333@gmail.com>
2024-08-07 19:00:29 +0530

    add pdf function

--- Project/OCR/ocr.py
+++ Project/OCR/ocr.py
@@ -1,7 +1,7 @@
 import pytesseract
 from PIL import Image
 
-def extract_text_from_image(image_path, lang='eng'):
+def image_to_text_tess(image_path, lang='eng'):
     """
     Extracts text from an image using Tesseract OCR.
 
@@ -21,6 +21,24 @@ def extract_text_from_image(image_path, lang='eng'):
     except Exception as e:
         return f"An error occurred: {e}"
 
+def pdf_to_text_tess(image, lang='eng'):
+    """
+    Extracts text from an image using Tesseract OCR.
+
+    Parameters:
+    - image (PIL.PngImagePlugin.PngImageFile): The path to the image file.
+    - lang (str): The language to use for OCR (default is 'eng' for English).
+
+    Returns:
+    - str: The text extracted from the image.
+    """
+    try:
+            # Use pytesseract to extract text
+        text = pytesseract.image_to_string(image, lang=lang)
+        return text
+    except Exception as e:
+        return f"An error occurred: {e}"
+
 # Example usage:
-text = extract_text_from_image(r'E:\Python projects\OCR_NLP\ocr_nlp_project\Project\Data\Images\photo_4_2024-01-17_12-42-32.jpg')
+text = image_to_text_tess(r'E:\Python projects\OCR_NLP\ocr_nlp_project\Project\Data\Images\photo_4_2024-01-17_12-42-32.jpg')
 print(text)
